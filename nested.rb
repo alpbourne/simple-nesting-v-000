4bd250e7c414890b6adde881364d519307026ee8
alpbourne <alpbourne@gmail.com>
2017-09-22 16:58:35 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -62,7 +62,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
+	programmer_hash[:yukihiro_matsumoto] = { :known_for => "Unix", :languages => ["LISP", "C"]}
 	programmer_hash =
  		{
         :grace_hopper => {
